8f35b7b3187f2ef4df70c5093e9b8e183aababdc
andrewSaver <andysaver@gmail.com>
2018-03-13 20:23:49 -0400

    version 7

--- dataTypes.js
+++ dataTypes.js
@@ -10,15 +10,51 @@
     
 // Comparisons with Primitives
 
-'andy' === 'andy';
-1 === 1;
-true === true;
-false === false;
-undefined === undefined;
-null === null;
+// 'andy' === 'andy'; true
+// 1 === 1; true
+// true === true;
+// false === false;
+// undefined === undefined;
+// null === null;
 
-// Comparisons with objects
+// // Comparisons with objects
 
-{ } === {} // false
-[1, 2, 3] === [1, 2, 3] // false
+// { } === { } // false
+// [1, 2, 3] === [1, 2, 3] // false
+
+// They are separate memory locations, therefore they are not equal.
+
+// Primitives are values
+// Objects are references (memory addresses)
+
+// How are primitives and objects stored inside of variables?
+
+var myPrimitive = 10;
+var myObject = { name: 'andy' };
+
+console.log(myPrimitive);
+console.log(myObject); // This is a memory address
+
+var myHouse = { color: 'blue' }; // Memory address
+myHouse.color = 'red'; // javascript will look inside the myHouse memory address and change the color property value.
+
+var myHouse = { color: 'blue' }; // creates memory address to store property color.
+var color = myHouse.color; // Go to myHouse memory address, append value 'blue'.
+color = 'red'; // changes the color variable property value, but not the myHouse.color property value.
+
+console.log(myHouse.color);
+
+var myHouse1 = { color: 'blue' };
+var myHouse2 = myHouse1; // this tells the computer that variable myHouse2 is the same as the myHouse1 object.
+myHouse2.color = 'red'; // since they are the same, changing the myHouse2.color property also changes myHouse1.
+
+console.log(myHouse1.color); // red
+console.log(myHouse2.color); // red
+
+var myHouse1 = { color: 'blue' };
+var myHouse2 = { color: 'blue' };
+myHouse2.color = 'red';
+
+console.log(myHouse1.color); // blue
+console.log(myHouse2.color); // red
 
