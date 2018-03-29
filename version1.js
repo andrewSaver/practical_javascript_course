6cd6f4f7bb026ff4eb915b7162d433818f977f8a
andrewSaver <andysaver@gmail.com>
2018-03-29 19:47:19 -0400

    version 1.2

--- version1.js
+++ version1.js
@@ -1,22 +1,29 @@
 // It should have a place to store todos
+// Variable is a 'nickname' so we can reference it later
 
-var todos = ['item 1', 'item 2', 'item 3']
+var todos = ['item 1', 'item 2', 'item 3'];
+// console.log(todos);
 
 // It should have a way to display todos
 
-console.log('My todos: ', todos);
+console.log(todos); // todos is not in quotes because it's not text, it's a variable that we made.
+console.log('My Todos:', todos);
 
 // It should have a way to add new todos
 
-todos.push('new item');
-console.log('My todos:', todos);
+todos.push('item 4');
+todos.push('item 5');
+console.log('My Todos: ', todos);
+
 
 // It should have a way to change a todo
 
-todos[0] = 'changed item';
-console.log(todos);
+todos[4] = 'new item'; // assign a new value
+console.log('My Todos: ', todos);
+
 
 // It should have a way to delete a todo
 
-todos.splice(3, 1);
-console.log(todos);
+todos.splice(4, 1);
+console.log('My Todos: ', todos);
+
