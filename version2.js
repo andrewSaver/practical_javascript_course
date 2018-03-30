9eade7f868acc3079e92f6fb68a61872ec61494e
andrewSaver <andysaver@gmail.com>
2018-03-29 20:22:18 -0400

    version 2.2

--- version2.js
+++ version2.js
@@ -1,35 +1,42 @@
 var todos = ['item 1', 'item 2', 'item 3'];
 
-// It should display todos.
+// function example(parameter){} / parameter is the same as argument
+// example(argument);
+
+// function sayHiTo(person) { // person = variable that will be given a value
+//     console.log('Hi', person);
+// }
+
+// sayHiTo('Andy'); // person is being set to 'Andy'
+
+// It should have a way to display todos.
 
 function displayTodos() {
-    console.log('My todos: ', todos);
+    console.log('My Todos: ', todos);
 }
 
 displayTodos();
 
-// It should have a function to add todos.
 
-function addTodo(todo) {
+// It should have a function to add todos.
+function addTodos(todo) {
     todos.push(todo);
     displayTodos();
 }
 
-addTodo('new item');
+addTodos('new item');
 
 
 //It should have a function to change todos.
-
 function changeTodo(position, newValue) {
     todos[position] = newValue;
     displayTodos();
 }
 
-changeTodo(3, 'updated todo');
+changeTodo(3, 'newer item');
 
 
 // It should have a function to delete todos.
-
 function deleteTodo(position) {
     todos.splice(position, 1);
     displayTodos();
